db20a894405f3113dd8cbebc53d305ce41f47650
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-03-09 11:01:26 +0300

    Initial commit

---
+++ src/main/java/main/isbd/IsbdApplication.java
@@ -0,0 +1,31 @@
+package main.isbd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@Slf4j
+public class IsbdApplication {
+	public static final Logger logger = LoggerFactory.getLogger(IsbdApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbdApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
+	}
+}
